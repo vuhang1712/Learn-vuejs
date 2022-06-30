d3041f81801e99dc5f57dff481d4f49d8761ada5
vuhang1712 <vuhang1939@gmail.com>
2022-06-30 21:21:02 +0700

    Day01

---
+++ Day_01/app.js
@@ -0,0 +1,32 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+    deleteGoal(goal) {
+      const index = this.goals.indexOf(goal);
+      this.goals.splice(index, 1);
+    },
+  },
+}).mount("#app");
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
